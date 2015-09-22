5840cde9448c9c14ef1e68f24c9017833dca6caa
Edgar Pabon <edgar.pabon@gmail.com>
2015-09-21 23:00:00 -0700

    Initial commit

---
+++ public/javascripts/app.js
@@ -0,0 +1,15 @@
+angular.module('airgrad', [
+  'ngRoute'
+  ])
+
+.config (function ($routeProvider, $httpProvider) {
+  $routeProvider
+    .when('/courses', {
+      templateUrl: '/views/courses.html',
+      controller: ''
+    })
+    .when('/welcome', {
+      templateUrl: '/views/welcome.html'
+    })
+    .otherwise('/welcome');
+});
